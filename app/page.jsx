a4d108e82912d34a53a2eb11d5fb6a8dd2a8f935
ncls-brn <nicolas.j.baran@gmail.com>
2024-06-11 15:04:03 +0200

    photo + cercle resume dowmload

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,60 @@
-export default function Home() {
-  return <main>Homepage</main>;
-}
+import Photo from "@/components/Photo";
+import Social from "@/components/Social";
+import Stats from "@/components/Stats";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+//import CV from "@/public/assets/resume/cv.pdf";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/*text*/}
+          <div className="text-center xl:text-left order-2 xl:order-none">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm
+              <br />
+              <span className="text-accent">Nicolas Baran</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I was able to experience through practice many tools and daily
+              gestures of developer but also several modes of operation. I take
+              part in projects in different domains, teams and languages, giving
+              me great adaptability.
+            </p>
+            {/*button social*/}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <a
+                href="/assets/resume/cv.pdf"
+                download
+                target="_blank"
+                rel="noopener noreferrer"
+                className="uppercase flex items-center gap-2"
+              >
+                <Button variant="outline" size="lg">
+                  <span>Download CV</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+              </a>
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
+              </div>
+            </div>
+          </div>
+          {/*photo*/}
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
+        </div>
+      </div>
+      <Stats />
+    </section>
+  );
+};
+
+export default Home;
---
+++ components/Social.jsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+const socials = [
+  { icon: <FaGithub />, path: "https://github.com/ncls-brn" },
+  { icon: <FaLinkedin />, path: "https://www.linkedin.com/in/nicolas-baran/" },
+];
+
+const Social = ({ containerStyles, iconStyles }) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, index) => {
+        return (
+          <Link
+            key={index}
+            href={item.path}
+            className={iconStyles}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {item.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Social;
