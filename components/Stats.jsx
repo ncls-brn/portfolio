f7ed170ffd02ea52511cf921d46f00c1d4d4193a
ncls-brn <nicolas.j.baran@gmail.com>
2024-06-13 11:16:40 +0200

    update stats component

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -4,7 +4,7 @@ import CountUp from "react-countup";
 
 const stats = [
   { num: 2, text: "Years of experience" },
-  { num: 8, text: "Projects completed" },
+  { num: 9, text: "Projects completed" },
   { num: 1000, texts: "more", text: "Code lines" },
   { num: 500, texts: "more", text: "Code commits" },
 ];
