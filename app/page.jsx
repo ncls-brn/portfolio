7cbce47f0409d425915e72abe829cd1fa88a3c88
ncls-brn <nicolas.j.baran@gmail.com>
2024-10-24 16:27:37 +0200

    update file page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -14,7 +14,7 @@ const Home = () => {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
-              Hello,I'm
+              Hello I'm
               <br />
               <span className="text-accent">Nicolas Baran</span>
             </h1>
