7e4dc6e06efb5ab2681b31c9193c2a87603d4763
ncls-brn <nicolas.j.baran@gmail.com>
2025-02-19 09:46:04 +0100

    update

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,12 +1,11 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { Repeat } from "lucide-react";
 import Image from "next/image";
 
 const Photo = () => {
   return (
-    <div className="w-full h-full relative">
+    <div className="w-full h-full relative flex items-center justify-center">
       <motion.div
         initial={{ opacity: 0 }}
         animate={{
@@ -14,13 +13,14 @@ const Photo = () => {
           transition: { delay: 2, duration: 0.4, ease: "easeIn" },
         }}
       >
+        {/* Image */}
         <motion.div
           initial={{ opacity: 0 }}
           animate={{
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
           }}
-          className="w-[400px] h-[400px] xl:w-[600px] xl:h-[600px] mix-blend-lighten absolute"
+          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
         >
           <Image
             src="/assets/image1.png"
@@ -31,17 +31,18 @@ const Photo = () => {
             className="object-contain"
           />
         </motion.div>
-        {/*circle*/}
+
+        {/* Circle */}
         <motion.svg
-          className="w-[420px] xl:w-[620px] h-[420px] xl:h-[620px]"
+          className="w-[600px] xl:w-[800px] h-[600px] xl:h-[800px]"
           fill="transparent"
-          viewBox="0 0 620 620"
+          viewBox="0 0 600 600"
           xmlns="http://www.w3.org/2000/svg"
         >
           <motion.circle
-            cx="310"
-            cy="310"
-            r="300"
+            cx="300"
+            cy="300"
+            r="290"
             stroke="#00ff99"
             strokeWidth="4"
             strokeLinecap="round"
