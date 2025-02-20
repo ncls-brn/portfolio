413b6bb716a7d130aec3e6178169e40aa2850a35
ncls-brn <nicolas.j.baran@gmail.com>
2025-02-20 08:47:15 +0100

    update page jsx

--- app/page.jsx
+++ app/page.jsx
@@ -12,7 +12,7 @@ const Home = () => {
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/*text*/}
           <div className="text-center xl:text-left order-2 xl:order-none">
-            <span className="text-xl">Cybersecurity engineer</span>
+            <span className="text-xl">Cybersecurity Engineer</span>
             <h1 className="h1 mb-6">
               Hello I'm
               <br />
